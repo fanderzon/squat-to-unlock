2ad805294d44c7c79f2808b66aed5dbc882e22c4
Anders Planting <anders.planting@widespace.com>
2016-02-20 16:15:20 +0100

    Fixed navbar for now

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -7,12 +7,14 @@ class GameList extends Component{
       <div>
         <nav className="navbar navbar-default navbar-fixed-top">
           <div className="container-fluid">
-            <ul className="nav navbar-nav">
-              <li className="pull-left"><a href="#">Profile</a></li>
-              <li className="navbar-text">Games</li>
-              <li><a href="#">
-                </a></li>
-              <li className="social pull-right"><a href="#">New game</a></li>
+            <ul className="nav navbar-nav navbar-left">
+              <li className="pull-left"><a href="#"><i className="fa fa-user"></i></a></li>
+              <li className="navbar-text">
+                Games
+              </li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right pull-right">
+              <li className="pull-right"><a href="#"><i className="fa fa-plus"></i></a></li>
             </ul>
           </div>
         </nav>
