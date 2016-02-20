4aa9866947a3f542ae3b2bc88d8d465de7213965
Anders Planting <anders.planting@widespace.com>
2016-02-20 21:37:20 +0100

    Styled nickname view

---
+++ src/components/Signup.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router';
+
+class Signup extends Component{
+  render() {
+    return (
+      <div>
+
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container-fluid">
+            <span className="navbar-brand text-center" style={{ width: 100 + '%'}} >Squat to unlock</span>
+          </div>
+        </nav>
+
+        <div className="row m-t-3">
+
+          <div className="col-md-12">
+
+            <form className="form-horizontal">
+
+              <div className="form-group col-md-12">
+                <label>Nickname</label>
+                <input className="form-control col-md-12" type="text" placeholder="Select nickname"></input>
+              </div>
+
+              <div className="form-group" style={{ marginBottom: 100 + 'px' }}>
+                <label className="col-md-12">Select avatar</label>
+                <div className="col-md-12">
+                  <label className="radio-inline text-center">
+                    <img src="./images/female_1.png" className="avatar"></img>
+                    <br />
+                    <input type="radio" name="avatar"></input>
+                  </label>
+                  <label className="radio-inline text-center">
+                    <img src="./images/male_1.png" className="avatar"></img>
+                    <br />
+                    <input type="radio" name="avatar"></input>
+                  </label>
+                  <label className="radio-inline text-center">
+                    <img src="./images/female_2.png" className="avatar"></img>
+                    <br />
+                    <input type="radio" name="avatar"></input>
+                  </label>
+                  <label className="radio-inline text-center">
+                    <img src="./images/male_2.png" className="avatar"></img>
+                    <br />
+                    <input type="radio" name="avatar"></input>
+                  </label>
+                </div>
+              </div>
+
+              <Link to="/games">
+                <div className="row footer bg-success new-game-footer">
+                  <div className="col-md-12 text-center">
+                    <p className="new-game-footer-text">
+                      Get started
+                    </p>
+                  </div>
+                </div>
+              </Link>
+
+
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Signup;
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,13 @@ import App from './components/App.js';
 import Main from './components/Main.js';
 import GameList from './components/GameList.js';
 import NewGame from './components/NewGame.js';
-
+import Signup from './components/Signup.js';
 
 module.exports = (
   <Route path="/" component={App}>
     <IndexRoute component={Main}/>
     <Route path="/games" component={GameList}></Route>
     <Route path="/new_game" component={NewGame}></Route>
+    <Route path="/signup" component={Signup}></Route>
   </Route>
 )
