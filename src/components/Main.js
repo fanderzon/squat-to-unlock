3231d2a0a7b08a0ff7891a6ff68926cede9a4ebb
Anders Planting <anders.planting@widespace.com>
2016-02-20 14:17:52 +0100

    Basic styling added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router';
 
+
 class Main extends Component{
   render() {
     return (
       <div>
-        <p>Squat to unlock</p>
-        <Link to="/games">Start</Link>
+        <div className="filler"></div>
+        <div className="container-fluid bg-dark">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="image-container">
+                <img src="./images/squat-icon.png" className="img-responsive center-block squat-icon" alt="Squat icon" />
+              </div>
+            </div>
+          </div>
+          <div className="row footer bg-dark">
+            <div className="col-md-12 text-center">
+              <p>
+                <h5>
+                  <Link to="/games">Squat to unlock</Link>
+                </h5>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
