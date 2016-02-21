7dee0f5a1e7624d35f8c46b45089b63e2b4ce9d9
Anders Planting <anders.planting@widespace.com>
2016-02-21 21:35:37 +0100

    Super basic squat animation.

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,21 @@ import { Link } from 'react-router';
 
 
 class Main extends Component{
+
+  init() {
+    this.setInterval(this.animateIcon, 1400);
+  }
+
+  animateIcon(){
+    var images = $('#squat_icon img'),
+        now    = images.filter(':visible'),
+        next   = now.next().length ? now.next() : images.first(),
+        speed  = 1000;
+
+    now.fadeOut(speed);
+    next.fadeIn(speed);
+  }
+
   render() {
     return (
       <div>
@@ -11,7 +26,9 @@ class Main extends Component{
           <div className="row">
             <div className="col-md-12">
               <div className="image-container">
-                <img src="./images/squat-icon.png" className="img-responsive center-block squat-icon" alt="Squat icon" />
+                <div id="cf">
+                  <img src="./images/squat-icon.gif" className="img-responsive center-block squat-icon" alt="Squat icon" />
+                </div>
               </div>
             </div>
           </div>
